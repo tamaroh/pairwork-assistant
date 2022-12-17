27a9237baeaf0fffd3a4b9d8be4ef6417eb64249
= <=>
2022-12-18 01:23:41 +0900

    added history

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const displayArea = document.getElementById("display");
 const setStudentsButton = document.getElementById("setstudents");
+const history = [];
 
 const getIndexAtRandom = (list) => {
     return Math.floor(Math.random()*list.length);
@@ -9,21 +10,31 @@ const createPair = (list) => {
     const tempList = [...list];
     while (tempList.length > 1) {
         const pair = [tempList[0]];
-        console.log(pair)
         tempList.splice(0, 1);
         const randomedIndex = getIndexAtRandom(tempList);
         pair.push(tempList[randomedIndex]);
         tempList.splice(randomedIndex,1)
         pairs.push(pair);
-        console.log(tempList.length)
     }
     if (tempList.length === 1) {
         pairs[0].push(tempList[0])
     }
     return pairs;
 }
+
+const showHistory = () => {
+    displayArea.innerHTML = "";
+    const historyElement = document.createElement("div");
+    history.forEach((element, index) => {
+        historyElement.innerHTML += `<h2>Day ${index + 1}</h2>`;
+        historyElement.append(JSON.stringify(element));
+    });
+    displayArea.append(historyElement);
+}
+
 setStudentsButton.addEventListener("click", () => {
     const students = document.getElementsByTagName("textarea")[0].value.split("\n");
     const createdPairs = createPair(students);
-    displayArea.append(JSON.stringify(createdPairs));
+    history.push(createdPairs);
+    showHistory();
 })
