2ebbaa4eb12f23cc5c512f821356c4a712e7590d
tamaroh <tamaroh@gmail.com>
2022-12-18 11:59:53 +0900

    added button, set unpairdMap

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const displayArea = document.getElementById("display");
 const setStudentsButton = document.getElementById("setstudents");
+const createdPairButton = document.getElementById("createpair");
+
+let students;
 const history = [];
+const unpairedMap = new Map();
 
 const getIndexAtRandom = (list) => {
     return Math.floor(Math.random()*list.length);
 }
 const createPair = (list) => {
     const pairs = [];
-    list.delete("");
     const tempList = Array.from(list);
     while (tempList.length > 1) {
         const pair = [tempList[0]];
@@ -16,13 +19,25 @@ const createPair = (list) => {
         pair.push(tempList[randomedIndex]);
         tempList.splice(randomedIndex,1)
         pairs.push(pair);
+        unpairedMap.get(pair[0]).delete(pair[1]);
+        unpairedMap.get(pair[1]).delete(pair[0]);
     }
     if (tempList.length === 1) {
         pairs[0].push(tempList[0])
     }
     return pairs;
 }
-
+const createUnpairedMap = (setOfStudents) => {
+    for (const student1 of setOfStudents) {
+            unpairedMap.set(student1, new Set());
+            console.log(unpairedMap)
+        for (const student2 of setOfStudents) {
+            console.log(unpairedMap.get(student1))
+            unpairedMap.get(student1).add(student2)
+        }
+    }
+    console.log(unpairedMap);
+}
 const showHistory = () => {
     displayArea.innerHTML = "";
     const historyElement = document.createElement("div");
@@ -34,8 +49,14 @@ const showHistory = () => {
 }
 
 setStudentsButton.addEventListener("click", () => {
-    const students = new Set(document.getElementsByTagName("textarea")[0].value.split("\n"));
+    students = new Set(document.getElementsByTagName("textarea")[0].value.split("\n"));
+    students.delete("");
+    createUnpairedMap(students);
+})
+
+createdPairButton.addEventListener("click", () => {
     const createdPairs = createPair(students);
     history.push(createdPairs);
     showHistory();
+    console.log(unpairedMap)
 })
